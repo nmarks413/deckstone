69a206936a17cdba0af664b32b3b6b487d2a4914
Rishi Nath <rishi.nath@gmail.com>
2018-11-30 14:58:51 -0800

    Improved formatting, added hero, cleaned up code.

--- deckstone.js
+++ deckstone.js
@@ -1,15 +1,15 @@
 const deckstring = require("deckstrings");
 const cardList = require("./cards.json");
 
+
 //Input deckstring:
-const deck = deckstring.decode("AAECAa0GDAmXArQD7QXTCtcKvsgC5swCws4CkNMCoIADjYIDCaEE0cEC2MEC5cwCtM4C8M8C6NAC4+kCn+sCAA==");
+deckcode = "AAECAa0GDAmXArQD7QXTCtcKvsgC5swCws4CkNMCoIADjYIDCaEE0cEC2MEC5cwCtM4C8M8C6NAC4+kCn+sCAA=="
+const deck = deckstring.decode(deckcode);
 
 //Getting names of dbfIds and storing # of copies:
 var orderDeck = [];
 for(var i=0; i<deck.cards.length; i++){
-  /*orderDeck.push("# " + deck.cards[i][1] + "x " + search(deck.cards[i][0]));*/
-  orderDeck.push([search(deck.cards[i][0]),deck.cards[i][1]
-  ]);
+  orderDeck.push([search(deck.cards[i][0]),deck.cards[i][1]]);
 };
 
 //Sorting in order of mana cost:
@@ -17,8 +17,16 @@ orderDeck.sort(function(a, b){
   return search(a[0])-search(b[0])
 });
 
-console.log(orderDeck);
+//Formatting and printing output:
+console.log('# ' + search(deck.heroes[0]));
+console.log('#')
 
+for(let i in orderDeck){
+  console.log('# ' + orderDeck[i][1] + 'x ' + orderDeck[i][0]);
+}
+
+console.log('#')
+console.log('# ' + deckcode)
 
 //Search function (Can return name for dbfId OR return mana cost for name)
 function search(card){
