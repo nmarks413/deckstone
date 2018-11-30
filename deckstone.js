f0d47b6b92e7505469f27e6cb6fe930efdde37af
Rishi Nath <rishi.nath@gmail.com>
2018-11-30 14:32:46 -0800

    Managed to document copies of cards and sort by mana cost by using two dimensional array instead of dictionary

--- deckstone.js
+++ deckstone.js
@@ -4,18 +4,21 @@ const cardList = require("./cards.json");
 //Input deckstring:
 const deck = deckstring.decode("AAECAa0GDAmXArQD7QXTCtcKvsgC5swCws4CkNMCoIADjYIDCaEE0cEC2MEC5cwCtM4C8M8C6NAC4+kCn+sCAA==");
 
-//Getting names of dbfIds:
+//Getting names of dbfIds and storing # of copies:
 var orderDeck = [];
 for(var i=0; i<deck.cards.length; i++){
   /*orderDeck.push("# " + deck.cards[i][1] + "x " + search(deck.cards[i][0]));*/
-  orderDeck.push(search(deck.cards[i][0]));
-}
+  orderDeck.push([search(deck.cards[i][0]),deck.cards[i][1]
+  ]);
+};
 
 //Sorting in order of mana cost:
-orderDeck.sort(function(a, b){return search(a)-search(b)})
-for(var i=0; i<orderDeck.length; i++){
-  console.log("# " + orderDeck[i]);
-}
+orderDeck.sort(function(a, b){
+  return search(a[0])-search(b[0])
+});
+
+console.log(orderDeck);
+
 
 //Search function (Can return name for dbfId OR return mana cost for name)
 function search(card){
