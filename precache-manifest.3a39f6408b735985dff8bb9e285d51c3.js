33f1fdf5383d02c919ebc5a47bf45c44012fbdd7
Rishi Nath <rishi.nath@gmail.com>
2019-01-17 11:45:10 -0800

    Updates

---
+++ precache-manifest.3a39f6408b735985dff8bb9e285d51c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9133693f3ec9e96ef1c2",
+    "url": "/deckstone/static/js/runtime~main.9133693f.js"
+  },
+  {
+    "revision": "32523ee4a530a9c13f00",
+    "url": "/deckstone/static/js/main.32523ee4.chunk.js"
+  },
+  {
+    "revision": "eff0581d9c71458bb0e3",
+    "url": "/deckstone/static/js/1.eff0581d.chunk.js"
+  },
+  {
+    "revision": "eff0581d9c71458bb0e3",
+    "url": "/deckstone/static/css/1.88b83987.chunk.css"
+  },
+  {
+    "revision": "99039a8b8067f6ffcde20d88580c6f99",
+    "url": "/deckstone/index.html"
+  }
+];
